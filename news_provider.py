2342446db467ccea47d39c19488129f72a402c67
NickyKlaus <nickolai.beloglazov@gmail.com>
2022-07-20 13:44:47 +0600

    Initial commit

---
+++ news_provider.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flasgger import Swagger, LazyJSONEncoder, swag_from
+from webargs import fields
+from webargs.flaskparser import use_args
+from newsapi import NewsApiClient
+
+app = Flask(__name__)
+api = Api(app)
+app.json_encoder = LazyJSONEncoder
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": '/',
+            "route": '/news_provider_schema.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+swagger = Swagger(app, template_file="news_provider_schema.json", config=swagger_config)
+
+
+class News(Resource):
+    # add you api key before using
+    __base_url = '''https://newsapi.org/v2/everything?q={}&sortBy=publishedAt&apiKey='''
+
+    @use_args(
+        {
+            'term': fields.Str(required=False, missing='Python'),
+            'page': fields.Int(required=False, missing=1)
+        },
+        location='querystring'
+    )
+    @swag_from("news_provider_schema.yaml", methods=['GET'])
+    def get(self, args):
+        if args['term'] == '_exit':
+            exit()
+        return self._get_news(search_term=args['term'], page=args['page'])
+
+    @staticmethod
+    def _get_news(search_term='nasa', page=1, page_size=10):
+        # add you api key before using
+        newsapi = NewsApiClient(api_key='')
+        news = newsapi.get_everything(q=search_term,
+                                      language='en',
+                                      page=page,
+                                      page_size=page_size)
+        return news.get('articles') if news.get('totalResults') > 0 else []
+
+
+api.add_resource(News, '/')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5005, debug=True)
+
